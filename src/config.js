da089fd4d5f0d2394842b650d29289986663520c
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-18 19:48:48 +0500

    client 3.1

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://cargo956.kz:3001',
+    
+    nameCargo: 'CARGO956'
+  };
+  
+  
+export default config;
+  
